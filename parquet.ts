b3b5d87f332529724b885b00dbd4c3db40ce8ee2
Wil Wade <wil.wade@unfinished.com>
2022-04-26 10:28:09 -0400

    Fix parquet.ts exports

--- parquet.ts
+++ parquet.ts
@@ -1,14 +1,12 @@
 import * as reader from './lib/reader';
-import writer from './lib/writer';
+import * as writer from './lib/writer';
 import * as schema from './lib/schema';
 import * as shredder from './lib/shred';
 
-module.exports = {
-  ParquetEnvelopeReader: reader.ParquetEnvelopeReader,
-  ParquetReader: reader.ParquetReader,
-  ParquetEnvelopeWriter: writer.ParquetEnvelopeWriter,
-  ParquetWriter: writer.ParquetWriter,
-  ParquetTransformer: writer.ParquetTransformer,
-  ParquetSchema: schema.ParquetSchema,
-  ParquetShredder: shredder,
-};
+export const  ParquetEnvelopeReader = reader.ParquetEnvelopeReader;
+export const  ParquetReader = reader.ParquetReader;
+export const  ParquetEnvelopeWriter = writer.ParquetEnvelopeWriter;
+export const  ParquetWriter = writer.ParquetWriter;
+export const  ParquetTransformer = writer.ParquetTransformer;
+export const  ParquetSchema = schema.ParquetSchema;
+export const  ParquetShredder = shredder;
