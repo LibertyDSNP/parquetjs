33eea8b18ddeff9f4b0350072d9a130b689bb265
Wil Wade <wil.wade@unfinished.com>
2023-05-23 13:39:04 -0400

    Better update system for simple snapshot

--- test/jsonSchema.test.ts
+++ test/jsonSchema.test.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { expect } from "chai";
 import { JSONSchema4 } from 'json-schema';
 import addressSchema from './test-files/address.schema.json';
@@ -9,41 +11,42 @@ import { ParquetSchema } from '../parquet';
 
 // Super Simple snapshot testing
 const checkSnapshot = (actual: any, snapshot: string, update = false) => {
-  const expected = require(snapshot);
   if (update) {
-    console.log(`Replace the contents of ${snapshot} with:\n`, JSON.stringify(JSON.parse(JSON.stringify(actual)), null, 2));
-    expect("See output").to.equal("");
+    fs.writeFileSync(path.resolve("test", snapshot), JSON.stringify(JSON.parse(JSON.stringify(actual)), null, 4));
+    expect(`Updated the contents of "${snapshot}"`).to.equal("");
   } else {
+    const expected = require(snapshot);
     expect(JSON.parse(JSON.stringify(actual))).to.deep.equal(expected);
   }
 }
 
 describe("Json Schema Conversion", function () {
+  const update = false;
   it("Simple Schema", function () {
     const js = addressSchema as JSONSchema4;
 
     const ps = ParquetSchema.fromJsonSchema(js);
-    checkSnapshot(ps, './test-files/address.schema.result.json');
+    checkSnapshot(ps, './test-files/address.schema.result.json', update);
   });
 
   it("Arrays", function () {
     const js = arraySchema as JSONSchema4;
 
     const ps = ParquetSchema.fromJsonSchema(js);
-    checkSnapshot(ps, './test-files/array.schema.result.json');
+    checkSnapshot(ps, './test-files/array.schema.result.json', update);
   });
 
   it("Objects", function () {
     const js = objectSchema as JSONSchema4;
 
     const ps = ParquetSchema.fromJsonSchema(js);
-    checkSnapshot(ps, './test-files/object.schema.result.json');
+    checkSnapshot(ps, './test-files/object.schema.result.json', update);
   });
 
   it("Nested Objects", function () {
     const js = objectNestedSchema as JSONSchema4;
 
     const ps = ParquetSchema.fromJsonSchema(js);
-    checkSnapshot(ps, './test-files/object-nested.schema.result.json');
+    checkSnapshot(ps, './test-files/object-nested.schema.result.json', update);
   });
 });
